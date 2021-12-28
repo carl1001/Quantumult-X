2660cdd0c336155fe232690b3d1080e94cec3174
carl1001 <65800368+carl1001@users.noreply.github.com>
2021-12-28 09:29:41 +0800

    Update getDdCookie.js

--- getDdCookie.js
+++ getDdCookie.js
@@ -1,15 +1,15 @@
 //const url = "https://dingtalk.jiandaoyun.com/dashboard/app/5e44e189409e0f0006a58a51/get_member_signature";
 
 if($request.url.indexOf("dingtalk" && "get_member_signature") != -1){
-    getHeaders("钉钉健康打卡", "cookie", "ddCookie");
-    getHeaders("钉钉健康打卡","token", "ddToken");
-    getHeaders("钉钉健康打卡", "id", "ddId")
+    getHeaders("钉钉健康打卡", "Cookie", "ddCookie");
+    getHeaders("钉钉健康打卡","X-CSRF-Token", "ddToken");
+    getHeaders("钉钉健康打卡", "X-Request-ID", "ddId")
 }
     
     
     
 function getHeaders(name, subName, key){
-    var currentC = $request.headers.Cookie;
+    var currentC = $request.headers.subName;
     var existC = key => $prefs.valueForKey(key);
     var create = (currentC, key) => $prefs.setValueForKey(currentC, key);
     if(currentC && existC(key)){
